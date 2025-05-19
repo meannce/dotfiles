34d0a29ff884d18fbe8cff8662178479ddc4ca4f
meannce <optional@tutanota.de>
2025-05-19 21:43:26 +0100

    fix: change monitor

--- dot_config/hypr/scripts/executable_random_wallpaper.sh
+++ dot_config/hypr/scripts/executable_random_wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# to-do: if hyprctl monitors != DP-1, then DP-2
 # i hate this, but otherwise wallpaper doesn't load
 sleep 1 
 
@@ -8,4 +9,4 @@ WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "DP-2,$WALLPAPER" # DP-2 = monitor ('hyperctl monitors')
+hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER" # DP-1 = monitor ('hyperctl monitors')
