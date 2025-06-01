a8d1c6ba8e2d3729a7ec1717558231ee132609d8
meannce <optional@tutanota.de>
2025-06-01 13:13:49 +0100

    fix: lazy display port fix

--- dot_config/hypr/scripts/executable_random_wallpaper.sh
+++ dot_config/hypr/scripts/executable_random_wallpaper.sh
@@ -9,4 +9,4 @@ WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER" # DP-1 = monitor ('hyperctl monitors')
+hyprctl hyprpaper wallpaper "DP-2,$WALLPAPER" # DP-2 = monitor ('hyperctl monitors')
