581649eeadd53bde1cebdc28380392fe415a607c
meannce <optional@tutanota.de>
2025-07-27 14:05:31 +0100

    feat!: rollback dynamic monitor selection

--- dot_config/hypr/scripts/executable_random_wallpaper.sh
+++ dot_config/hypr/scripts/executable_random_wallpaper.sh
@@ -3,8 +3,8 @@ sleep 1
 
 WALLPAPER_DIR="$HOME/wallpapers"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-#MONITOR=$(hyprctl monitors | grep -m1 "Monitor" | awk '{print $2}')
+MONITOR=$(hyprctl monitors | grep -m1 "Monitor" | awk '{print $2}')
 
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "DP-2,$WALLPAPER"
+hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
