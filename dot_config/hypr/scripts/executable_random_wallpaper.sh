d2e3507050571981acc4055c95e927e4c9a61946
meannce <optional@tutanota.de>
2025-07-27 13:57:26 +0100

    hotfix(sleep): was unable to login

--- dot_config/hypr/scripts/executable_random_wallpaper.sh
+++ dot_config/hypr/scripts/executable_random_wallpaper.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+sleep 1
+
 WALLPAPER_DIR="$HOME/wallpapers"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 MONITOR=$(hyprctl monitors | grep -m1 "Monitor" | awk '{print $2}')
-
-until hyprctl hyprpaper listloaded &>/dev/null; do
-	sleep 0.1
-done
 
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$WALLPAPER"
